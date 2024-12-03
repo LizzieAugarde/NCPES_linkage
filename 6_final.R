d189a5ca7424a3b3915f107e4170efa07e85d129
LizzieAugarde <lizzie994@googlemail.com>
2024-12-03 15:37:18 +0000

    Added final cleaning and write out script

---
+++ 6_final.R
@@ -0,0 +1,37 @@
+##### NCPES_linkage 
+
+## 6. Final cleaning and write out
+
+##### step 1 - dropping variables and capitalising
+drop_vars <- function(x) {
+  x |>
+    subset(x, select = c(CPES_PRN, CPES_TRUST_CODE, ATTUM_PATIENTID, 
+                         ATTUM_TUMOURID, MERGE, FLAGICD104, FLAGICD103, 
+                         TUMOUR_LINKFLAG_COMBINED, DIFFDIAGDISCH, PATMATCH, 
+                         FINAL_UNIQUE, FLAG_RELATED_MATCH, 
+                         FLAG_RELATED_MATCH_UNIQ, FINAL_UNIQUE_EXTRA))
+}
+
+
+##### step 2 - capitalise variable names 
+capitalise <- function(x) {
+  names(x) <- toupper(names(x))
+}
+
+
+##### step 3- extract the data frames from the dataset list for write out
+dfs_from_list(dataset_list)
+
+
+##### step 4 - write out 
+filepath <- paste0(Sys.getenv('ncpes_linkage_drive'), "_", year)
+
+resps_filename <- paste0("NC_", year, "_LINKAGE_RES_", cas_snapshot, ".xlsx")
+nonresps_filename <- paste0("NC_", year, "_LINKAGE_NONRES_", cas_snapshot, ".xlsx")
+u16_resps_filename <- paste0("NC_U16_", year, "_LINKAGE_RES_", cas_snapshot, ".xlsx")
+u16_nonresps_filename <- paste0("NC_U16_", year, "_LINKAGE_NONRES_", cas_snapshot, ".xlsx")
+
+write.xlsx(resps_raw, file = paste0(filepath, "/", resps_filename), rowNames = FALSE)
+write.xlsx(nonresps_raw, file = paste0(filepath, "/", nonresps_filename), rowNames = FALSE)
+write.xlsx(u16_resps_raw, file = paste0(filepath, "/", u16_resps_filename), rowNames = FALSE)
+write.xlsx(u16_nonresps_raw, file = paste0(filepath, "/", u16_nonresps_filename), rowNames = FALSE)
