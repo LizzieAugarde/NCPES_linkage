f6e02b631a531760649fb02b241e87e0304262c7
LizzieAugarde <lizzie994@googlemail.com>
2024-12-03 15:48:56 +0000

    Changed how CAS snapshot is defined and used, added folder creation step

--- 0_setup.R
+++ 0_setup.R
@@ -7,22 +7,22 @@ library(NDRSAfunctions)
 library(tidyverse)
 library(dplyr)
 
-# put the NCPES linkage filepath into your renviron
+
+# step 1 - inputs 
 year <- dlgInput("What year of NCPES data are you linking? YYYY", Sys.info()["year"])$res
+cas_snapshot <- dlgInput("Which CAS snapshot are you linking to? Format should be 'cas2XXX'")$res
+analyst <- dlgInput("What is your CAS username? Format should be 'analysisfirstnamesurname'")$res
 
 
-# put inputs into the inputs.csv file in the folder
-inputvalues <- read.csv("inputs.csv") ##for RAP process development only 
-#####inputvalues <- read.csv(paste0(Sys.getenv('ncpes_linkage_drive'), "_", year, '/inputs.csv'))
-cas_snapshot <- inputvalues[inputvalues$input == "cas_snapshot", 2]
-analyst <- inputvalues[inputvalues$input == "cas_username", 2]
+# step 2 - creating a folder for this year's linkage outputs  
+dir.create(paste0(ncpes_linkage_drive, "/Linkage ", year))
 
 
-# CAS connection
-cas <- createConnection(username = analyst, port = 1525, sid = paste0("cas", cas_snapshot))
+# step 3 - CAS connection
+cas <- createConnection(username = analyst, port = 1525, sid = cas_snapshot)
 
 
-# Function to extract data frames from list 
+# step 4 - create function to extract data frames from list 
 dfs_from_list <- function(dataset_list) {
   for (name in names(dataset_list)) {
     assign(name, dataset_list[[name]], envir = .GlobalEnv)
